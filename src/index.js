04ac68df793da10a85cc1628579d8adb87447013
Paolo <scottypipz@gmail.com>
2019-08-29 11:08:47 +0800

    added Header and color variables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './components/widgets/modal'
 import './components/widgets/my-button'
 import './components/widgets/my-card'
 import './components/widgets/form-group'
+import './components/widgets/form-group-textarea'
 import './components/fragments/header'
 
 require('./styles/app.css')
@@ -22,18 +23,20 @@ template.innerHTML = `
         main {
             --my-primary: rgba(45, 156, 219, 0.90);
             --my-secondary: #2D9CDB;
+            --my-pink: #FFB3C3;
             --my-font-white: #ffffff;
             --my-font-black: rgba(0, 0, 0, 0.87);
             --my-font-black-secondary: rgba(0, 0, 0, 0.54);
             --my-red: #EB5757;
+            --my-black: #131313;
             --my-green: #219653;
           
             --my-font-shadow-dark: 0px 0px 10px rgba(0, 0, 0, 0.25);
             --my-font-shadow-light: 0px 0px 10px rgba(0, 0, 0, 0.25);
         }
     </style>
-
     <my-header></my-header>
+
     <main>
         <trello-column-list></trello-column-list>
     </main>
